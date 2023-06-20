9311292a9c409e65ff5a01312df8dc90ac3738d3
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-06-19 22:17:24 -0700

    add author editing views

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,9 @@ import datetime
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.decorators import login_required, permission_required 
 
@@ -98,3 +99,19 @@ def renew_book_librarian(request, pk):
   }
 
   return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(CreateView):
+  model = Author
+  fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+  initial = {'date_of_death': '11/06/2020'}
+
+
+class AuthorUpdate(UpdateView):
+  model = Author
+  fields = '__all__'
+
+
+class AuthorDelete(DeleteView):
+  model = Author
+  success_url = reverse_lazy('authors')
